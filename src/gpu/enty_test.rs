0a1f85ff7517185341620ccb80dddfed769e3c23
guodayang999 <33561649+guodayang999@users.noreply.github.com>
2021-08-06 17:36:22 +0800

    add multiexp test

---
+++ src/gpu/enty_test.rs
@@ -0,0 +1,15 @@
+use rust_gpu_tools::*;
+
+
+
+#[cfg(feature = "gpu")]
+#[test]
+pub fn test_multiexp_3080() {
+    println!("run test multiexp 3080")
+}
+
+#[cfg(feature = "gpu")]
+#[test]
+pub fn test_multiexp_2080ti() {
+    println!("run test multiexp 2080ti")
+}
