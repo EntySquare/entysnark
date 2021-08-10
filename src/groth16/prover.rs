c6462c4fd5c05135bb61b3c7a0b3ab569d2e179b
kenwxp <kenwxp007@163.com>
2021-08-11 01:21:16 +0800

    add log

--- src/groth16/prover.rs
+++ src/groth16/prover.rs
@@ -777,7 +777,6 @@ where
     C: Circuit<E> + Send,
 {
     let start = Instant::now();
-    let mut times = 1;
     let provers = circuits
         .into_par_iter()
         .map(|circuit| -> Result<_, SynthesisError> {
@@ -785,10 +784,9 @@ where
             prover.alloc_input(|| "", || Ok(E::Fr::one()))?;
 
             let par_now = Instant::now();
-            println!("===[{}]=== prover.create_proof_batch_priority_inner: circuit.synthesize start...",times);
+            println!("prover.create_proof_batch_priority_inner: circuit.synthesize start...",times);
             circuit.synthesize(&mut prover)?;
             println!("===[{}]=== prover.create_proof_batch_priority_inner: circuit.synthesize end cost: {:?}",times, par_now.elapsed());
-            times +=1;
 
             for i in 0..prover.input_assignment.len() {
                 prover.enforce(|| "", |lc| lc + Variable(Index::Input(i)), |lc| lc, |lc| lc);
