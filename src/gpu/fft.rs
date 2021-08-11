26c3e8ad53c54c8f979656e58d570b25c681710b
kenwxp <kenwxp007@163.com>
2021-08-12 01:10:43 +0800

    add log

--- src/gpu/fft.rs
+++ src/gpu/fft.rs
@@ -7,6 +7,7 @@ use ff::Field;
 use log::info;
 use rust_gpu_tools::*;
 use std::cmp;
+use std::time::Instant;
 
 const LOG2_MAX_ELEMENTS: usize = 32; // At most 2^32 elements is supported.
 const MAX_LOG2_RADIX: u32 = 8; // Radix256
