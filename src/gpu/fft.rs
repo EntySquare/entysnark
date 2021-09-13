bf6e3703d7ac6cfd1a0dcdfa32e9cb447bac352f
kenwxp <kenwxp007@163.com>
2021-09-14 05:57:04 +0800

    modify fftkernel for get GPU by bus_id

--- src/gpu/fft.rs
+++ src/gpu/fft.rs
@@ -29,14 +29,17 @@ where
 {
     pub fn create(priority: bool) -> GPUResult<FFTKernel<E>> {
         let lock = locks::GPULock::lock();
+        let lock_id = lock.id();
 
         let devices = opencl::Device::all();
+
         if devices.is_empty() {
             return Err(GPUError::Simple("No working GPUs found!"));
         }
 
         // Select the first device for FFT
-        let device = devices[0].clone();
+        // let device = devices[0].clone();
+        let device = devices.into_iter().filter(|d| d.bus_id().unwrap() == lock_id).next().unwrap().clone();
 
         let src = sources::kernel::<E>(device.brand() == opencl::Brand::Nvidia);
 
